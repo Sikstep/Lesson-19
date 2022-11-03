1a1d57da9eaf9d813455e31509d6f1ca233d0508
Kosty <Sikstep1988@gmail.com>
2022-11-03 16:22:17 +0300

    2nd part

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 'use strict';
-
+// 1st part
 let numberOfFilms;
 
 const answer = +prompt('Сколько фильмов вы уже посмотрели?', '');
-console.log('answer=', answer);
+console.log('answer =', answer);
+
+// 2nd part
+
+const personalMovieDB = {
+    count: answer,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
 
