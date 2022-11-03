7d39775c8f20943bee0cadb33b44c3188619c65f
Kosty <Sikstep1988@gmail.com>
2022-11-03 19:05:27 +0300

    3rd part1

--- js/script.js
+++ js/script.js
@@ -22,10 +22,17 @@ const userRatingFilm1 = +prompt('На сколько оцените его?', ''
 const nameLastFilmWatching2 = prompt('Один из последних просмотренных фильмов?', '');
 const userRatingFilm2 = +prompt('На сколько оцените его?', '');
 
-const movies = {
-    answerFilm1: nameLastFilmWatching1,
-    answerRatingFilm1: userRatingFilm1,
-    answerFilm2: nameLastFilmWatching2,
-    answerRatingfilm2: userRatingFilm2
-};
+// const movies = {
+//     answerFilm1: nameLastFilmWatching1,
+//     answerRatingFilm1: userRatingFilm1,
+//     answerFilm2: nameLastFilmWatching2,
+//     answerRatingfilm2: userRatingFilm2
+// };
+personalMovieDB.movies.answerFilm1 = nameLastFilmWatching1;
+personalMovieDB.movies.answerRatingFilm1 = userRatingFilm1;
+personalMovieDB.movies.answerFilm2 = nameLastFilmWatching2;
+personalMovieDB.movies.answerRatingFilm2 = userRatingFilm2;
+
+console.log(personalMovieDB.movies)
+
 console.log('whole answer =', movies);
