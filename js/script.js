b228d6275f29ccbbac9bcb564c81af6d5f202589
Kosty <Sikstep1988@gmail.com>
2022-11-03 19:19:59 +0300

    3rd part3

--- js/script.js
+++ js/script.js
@@ -28,10 +28,9 @@ const userRatingFilm2 = +prompt('На сколько оцените его?', ''
 //     answerFilm2: nameLastFilmWatching2,
 //     answerRatingfilm2: userRatingFilm2
 // };
-personalMovieDB.movies.answerFilm1 = nameLastFilmWatching1;
-personalMovieDB.movies.answerRatingFilm1 = userRatingFilm1;
-personalMovieDB.movies.answerFilm2 = nameLastFilmWatching2;
-personalMovieDB.movies.answerRatingFilm2 = userRatingFilm2;
+personalMovieDB.movies.nameLastFilmWatching1 = userRatingFilm1;
+personalMovieDB.movies.nameLastFilmWatching2 = userRatingFilm2;
+
 
 console.log(personalMovieDB.movies);
 
