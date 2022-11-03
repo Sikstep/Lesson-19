5bd15bf38bb474136851cc273f7900c88f12eeb4
Kosty <Sikstep1988@gmail.com>
2022-11-03 18:40:28 +0300

    3rd part

--- js/script.js
+++ js/script.js
@@ -1,7 +1,31 @@
 'use strict';
+// 1st part
 
-let numberOfFilms;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+console.log('answer =', numberOfFilms);
 
-const answer = +prompt('Сколько фильмов вы уже посмотрели?', '');
-console.log('answer=', answer);
+// 2nd part
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+// 3rd part
+
+const nameLastFilmWatching1 = prompt('Один из последних просмотренных фильмов?', '');
+const userRatingFilm1 = +prompt('На сколько оцените его?', '');
+
+const nameLastFilmWatching2 = prompt('Один из последних просмотренных фильмов?', '');
+const userRatingFilm2 = +prompt('На сколько оцените его?', '');
+
+const movies = {
+    answerFilm1: nameLastFilmWatching1,
+    answerRatingFilm1: userRatingFilm1,
+    answerFilm2: nameLastFilmWatching2,
+    answerRatingfilm2: userRatingFilm2
+};
+console.log('whole answer =', movies);
